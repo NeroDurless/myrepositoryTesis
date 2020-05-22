71bdd21a1f334de19b38b696656024cd49c85924
Jonmlr <Jonmlr@users.noreply.github.com>
2020-05-22 13:57:08 -0400

    Login centrado

--- login.php
+++ login.php
@@ -5,16 +5,27 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Página de logeo</title>
 <link rel="stylesheet" type="text/css" href="style.css">
+<style type="text/css">
+form {
+	color: #333;
+	font-family: "Comic Sans MS", cursive;
+	font-size: 20px;
+}
+</style>
 <script src="js/live.js"></script>
 </head>
+
+
 
 <body>
 
   <div class="login-page">
-    <p>Sistema de Registro de Pacientes y Control de Citas</p>
   <div class="form">
  
   <form action="comprueba_registro.php" method="post" class="register-form">
+      Sistema de Registro de Pacientes y Control de Citas
+      <p></p>
+
  
       <input type="text" name="usu" id="usu" placeholder="Usuario"/>
       <input type="password" name="contra" id="contra" placeholder="Contraseña"/>
@@ -27,22 +38,25 @@
 
 
     <form action="comprueba_login.php" method="post" class="login-form">
+    Sistema de Registro de Pacientes y Control de Citas
+    <p></p>
       
         <input type="text" name="login" id="login" placeholder="Usuario"/>
-        <input type="password" name="password" id="password" placeholder="Contraseña"/>
-      
+          <input type="password" name="password" id="password" placeholder="Contraseña"/>
+          
+        
         <button>login</button>
       
-        <p class="message">¿No te has registrado? <a href="#">Crea una cuenta</a></p>
+      <p class="message">¿No te has registrado? <a href="#">Crea una cuenta</a></p>
         
     </form>
     
   </div>
 </div>
 
-    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
-    <script  src="index.js"></script>
+<script  src="index.js"></script>
 
 </body>
 
